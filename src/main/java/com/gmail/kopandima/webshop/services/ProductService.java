c8521d04c62068acef46dff32ecd3cf83c69b43f
Kopan Dmytro <kopandima@gmail.com>
2019-05-22 09:10:39 +0200

    chore(ProductService): Added

---
+++ src/main/java/com/gmail/kopandima/webshop/services/ProductService.java
@@ -0,0 +1,19 @@
+package com.gmail.kopandima.webshop.services;
+
+import com.gmail.kopandima.webshop.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+
+}
