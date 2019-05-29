000eae5bc187ad43cd34c4a9081d5f6161923514
Kopan Dmytro <kopandima@gmail.com>
2019-05-29 11:09:08 +0200

    chore(OOD): Interface in the middle, ProductService added

--- src/main/java/com/gmail/kopandima/webshop/services/ProductService.java
+++ src/main/java/com/gmail/kopandima/webshop/services/ProductService.java
@@ -1,47 +1,15 @@
 package com.gmail.kopandima.webshop.services;
 
 import com.gmail.kopandima.webshop.models.Product;
-import com.gmail.kopandima.webshop.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
 
+public interface ProductService {
+    Iterable<Product> findAll();
 
-@Service
-public class ProductService {
+    Product findById(Integer id);
 
-    private final ProductRepository productRepository;
+    void save(Product product);
 
+    void update(Product product);
 
-    @Autowired
-    public ProductService(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
-
-
-    public Iterable<Product> findAll() {
-        return productRepository.findAll();
-    }
-
-    public Product findById(Integer id) {
-        if (productRepository.findById(id).isPresent()) {
-            return productRepository.findById(id).get();
-        } else {
-            System.out.println("No such entity with id");
-            return null;
-        }
-    }
-
-    public void save(Product product) {
-        productRepository.save(product);
-    }
-
-    public void update(Product product) {
-        productRepository.save(product);
-    }
-
-    public void delete(Product product) {
-        productRepository.delete(product);
-    }
-
+    void delete(Product product);
 }
---
+++ src/main/java/com/gmail/kopandima/webshop/services/ProductServiceImpl.java
@@ -0,0 +1,43 @@
+package com.gmail.kopandima.webshop.services;
+
+import com.gmail.kopandima.webshop.models.Product;
+import com.gmail.kopandima.webshop.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService  {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Iterable<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Product findById(Integer id) {
+        if (productRepository.findById(id).isPresent()) {
+            return productRepository.findById(id).get();
+        } else {
+            System.out.println("No such entity with id");
+            return null;
+        }
+    }
+
+    public void save(Product product) {
+        productRepository.save(product);
+    }
+
+    public void update(Product product) {
+        productRepository.save(product);
+    }
+
+    public void delete(Product product) {
+        productRepository.delete(product);
+    }
+
+}
