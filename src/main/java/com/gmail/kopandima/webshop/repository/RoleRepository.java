fc877f392755c1c81450b63919996fe53c46e6b4
Kopan Dmytro <kopandima@gmail.com>
2019-06-10 11:33:25 +0200

    chore(RoleRepository): CrudRepository for Role

---
+++ src/main/java/com/gmail/kopandima/webshop/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.gmail.kopandima.webshop.repository;
+
+import com.gmail.kopandima.webshop.models.Role;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoleRepository extends CrudRepository<Role,Long> {
+}
